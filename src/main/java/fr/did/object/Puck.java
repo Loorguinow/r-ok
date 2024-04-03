00ff81a4f95f08f7d448256cf4d49b0e369fdb03
Alexandre L <alexandre-lerousseau@etud.univ-tln.fr>
2024-04-03 16:50:04 +0200

    Ajoute la collision

--- src/main/java/fr/did/object/Puck.java
+++ src/main/java/fr/did/object/Puck.java
@@ -1,6 +1,9 @@
 package fr.did.object;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.material.Material;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
@@ -18,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Puck {
 
+    @Setter(AccessLevel.NONE)
     private static int idCounter = 0;
 
     private int id;
@@ -27,19 +31,34 @@ public class Puck {
     private Node node;
     @Setter(AccessLevel.NONE)
     private float size = 100; //percentage
+    private float mass = 10.0f;
+
+    private CollisionShape collisionShape;
+    private RigidBodyControl rigidBodyControl;
 
     public static Puck of(String form, Node node, AssetManager assetManager, boolean spawnOrNot) throws FormException {
         Puck puck = new Puck(node, assetManager);
-        Puck.formCheck(form, puck);
-        if (spawnOrNot) puck.spawnPuck();
+        puck.constructPhysicalPuck(form, spawnOrNot);
         return puck;
     }
 
     public static Puck of(String form, Node node, AssetManager assetManager, boolean spawnOrNot, int size) throws FormException {
         Puck puck = new Puck(node, assetManager, size);
-        Puck.formCheck(form, puck);
-        if (spawnOrNot) puck.spawnPuck();
+        puck.constructPhysicalPuck(form, spawnOrNot);
         return puck;
+    }
+
+    /**
+     * Construit le palet et le fait potentiellement apparaître
+     * @param form forme du palet
+     * @param spawnOrNot Apparition ou non du palet
+     */
+    private void constructPhysicalPuck(String form, boolean spawnOrNot) throws FormException {
+        Puck.formConstruct(form, this);
+        this.collisionShape = CollisionShapeFactory.createBoxShape(this.getGeometry());
+        this.rigidBodyControl = new RigidBodyControl(this.getCollisionShape(), this.getMass());
+        this.geometry.addControl(this.rigidBodyControl);
+        if (spawnOrNot) this.spawnPuck();
     }
 
     private Puck(Node node, AssetManager assetManager) {
@@ -60,7 +79,7 @@ public class Puck {
      * @param form Forme du palet
      * @param puck Instance de Puck pour créer la géométrie
      */
-    public static void formCheck(String form, Puck puck) throws FormException{
+    public static void formConstruct(String form, Puck puck) throws FormException{
 
         if (form.equals("cylinder")) puck.createCylinder();
         else if (form.equals("prism")) puck.createPrism();
