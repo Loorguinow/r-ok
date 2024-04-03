87dde9165d9c75782db4081a71c736a0ee7ffd4f
SEGURA LUCAS <lsegura630@u-001-13.univ-tln.fr>
2024-04-03 16:47:12 +0200

    Avancez majeur dans la creation de la table

--- src/main/java/fr/did/object/AirHockeyTable.java
+++ src/main/java/fr/did/object/AirHockeyTable.java
@@ -4,53 +4,103 @@ import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class AirHockeyTable extends SimpleApplication {
-    private Integer pos_x;
-    private Integer pos_y;
-    private Integer pos_z;
 
-    private Integer largeur;
-    private Integer longueur;
+    private Float largeur;
+    private Float longueur;
+
+    private Node node;
 
     public static void main(String[] args) {
-        AirHockeyTable app = new AirHockeyTable();
-        app.start();
+        Node root = new Node();
+        AirHockeyTable Air = new AirHockeyTable(5f,2f,root);
+        Air.start();
     }
 
     public void CreateTable() {
-        // Créer un rectangle avec une largeur et une longueur spécifiées
-        Quad quad = new Quad(this.longueur, this.largeur);
-        Geometry rectangle = new Geometry("Rectangle", quad);
 
-        // Créer un matériau pour le rectangle
-        Material material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        material.setColor("Color", ColorRGBA.White); // Couleur du rectangle (ici, blanc)
-
-        // Appliquer le matériau au rectangle
-        rectangle.setMaterial(material);
-
-        // Positionner le rectangle à un emplacement spécifique
-        rectangle.setLocalTranslation(new Vector3f(this.pos_x, this.pos_y, this.pos_z));
-
-        // Ajouter le rectangle à la scène
-        rootNode.attachChild(rectangle);
+        // Créer le sol
+        Box groundMesh = new Box(this.largeur , 0.1f, this.longueur );
+        Geometry groundGeo = new Geometry("Ground", groundMesh);
+        Material groundMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        groundMat.setColor("Color", ColorRGBA.White);
+        groundGeo.setMaterial(groundMat);
+        rootNode.attachChild(groundGeo);
 
 
+        // Créer les murs autour de la table
+
+        Material tableMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        tableMat.setColor("Color", ColorRGBA.Gray);
+
+        Box wallMesh = new Box(0.1f, 1f, ((this.longueur/3f)));
+        Geometry wallGeo1 = new Geometry("Wall1", wallMesh);
+        Geometry wallGeo15 = new Geometry("Wall1", wallMesh);
+
+        wallGeo1.setMaterial(tableMat);
+        wallGeo15.setMaterial(tableMat);
+
+        wallGeo1.setLocalTranslation(-(this.largeur), 0f, (this.longueur-(this.longueur/3f)));
+        wallGeo15.setLocalTranslation(-(this.largeur), 0f, -(this.longueur-(this.longueur/3f)));
+
+        Geometry wallGeo2 = new Geometry("Wall2", wallMesh);
+        Geometry wallGeo25 = new Geometry("Wall2.5", wallMesh);
+
+
+        wallGeo2.setMaterial(tableMat);
+        wallGeo25.setMaterial(tableMat);
+
+        wallGeo2.setLocalTranslation((this.largeur), 0f, -(this.longueur-(this.longueur/3f)));
+        wallGeo25.setLocalTranslation((this.largeur), 0f, this.longueur-(this.longueur/3f));
+
+
+
+        Box wallMesh2 = new Box(this.largeur, 1f, 0.1f);
+        Geometry wallGeo3 = new Geometry("Wall3", wallMesh2);
+        Geometry wallGeo4 = new Geometry("Wall4", wallMesh2);
+
+        wallGeo3.setMaterial(tableMat);
+        wallGeo4.setMaterial(tableMat);
+
+        wallGeo3.setLocalTranslation(0f, 0f ,this.longueur);
+        wallGeo4.setLocalTranslation(0f, 0f,-(this.longueur));
+
+        rootNode.attachChild(wallGeo1);
+        rootNode.attachChild(wallGeo15);
+
+        rootNode.attachChild(wallGeo2);
+        rootNode.attachChild(wallGeo25);
+
+        rootNode.attachChild(wallGeo3);
+        rootNode.attachChild(wallGeo4);
+
+        // Ajouter la cage
+        Box cageMesh = new Box(0.1f, 1f, (this.longueur/ 3f)); // Taille de la cage
+
+        Geometry cageGeo1 = new Geometry("Cage", cageMesh);
+        Material cageMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        cageMat.setColor("Color", ColorRGBA.Red); // Couleur noire pour la cage
+        cageGeo1.setMaterial(cageMat);
+        cageGeo1.setLocalTranslation(-(this.largeur), 0f, 0); // Positionnée au milieu des murs
+        rootNode.attachChild(cageGeo1);
+
+        Geometry cageGeo2 = new Geometry("Cage", cageMesh);
+        cageMat.setColor("Color", ColorRGBA.Red); // Couleur noire pour la cage
+        cageGeo2.setMaterial(cageMat);
+        cageGeo2.setLocalTranslation(this.largeur, 0f, 0f); // Positionnée au milieu des murs
+        rootNode.attachChild(cageGeo2);
     }
 
     @Override
     public void simpleInitApp() {
-        // Définir les paramètres de position et de taille de la table
-        this.pos_x = 0;
-        this.pos_y = 0;
-        this.pos_z = 0;
-        this.largeur = 3; // Largeur de la table
-        this.longueur = 5; // Longueur de la table
-
         // Créer la table de air hockey
-        CreateTable();
+        this.CreateTable();
     }
 
 }
