35592d34526c536ef1d263829f475ba802667d99
Alexandre L <alexandre-lerousseau@etud.univ-tln.fr>
2024-04-25 15:16:16 +0200

    Factorise les objets physiques pour implanter les bonus immobiles

--- src/main/java/fr/did/object/MobileObjectForm.java
+++ src/main/java/fr/did/object/MobileObjectForm.java
@@ -1,8 +1,8 @@
 package fr.did.object;
-public class ObjectForm {
+public class MobileObjectForm {
 
     public static final String CYLINDER = "cylinder";
     public static final String PRISM = "prism";
 
-    private ObjectForm() {}
+    private MobileObjectForm() {}
 }
